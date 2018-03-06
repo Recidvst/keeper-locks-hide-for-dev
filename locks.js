165c496a4d8fffa744a5ae59f6334c8360b253e0
Chris Snowden <cdsnowden@gmail.com>
2018-03-06 12:29:37 +0000

    checking for test env from url

--- locks.js
+++ locks.js
@@ -1,9 +1,10 @@
 (function (unlock) {
-
+  
+  const devList = ['.dev', '.test', '.staging'];
   const locks = [...document.getElementsByTagName('keeper-lock')]; // array from collection
   const location = window.location.href;
-  
-  if ( location.includes('.test') ) { // test env
+   
+  if ( devList.some( dev => location.includes(dev) ) ) { // url contains a dev string
   
     locks.forEach(function(lock) { // hide
       lock.style.display = 'none';
